ef43997a9371a573183e1a4991f4ba1ee70e4d7a
davidfitz314 <davidfitz314@gmail.com>
2020-08-03 17:58:23 -0600

    Updates

---
+++ precache-manifest.a3aba0ba2c989abfec752ba38ec4d250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83635588b295495c0bfbefded43e0f3d",
+    "url": "/HSK-Learn/index.html"
+  },
+  {
+    "revision": "ee9eb79923c9761fdc70",
+    "url": "/HSK-Learn/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e720e82054c52059fbf",
+    "url": "/HSK-Learn/static/js/2.3596974f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HSK-Learn/static/js/2.3596974f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9eb79923c9761fdc70",
+    "url": "/HSK-Learn/static/js/main.f812b1bf.chunk.js"
+  },
+  {
+    "revision": "8ef074ca05aef3c58dee",
+    "url": "/HSK-Learn/static/js/runtime-main.5a7c36d5.js"
+  }
+]);
