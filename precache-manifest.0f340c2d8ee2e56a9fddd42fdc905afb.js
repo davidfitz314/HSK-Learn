8be30db002d5d505c4ef026979ca552e86e696c0
davidfitz314 <davidfitz314@gmail.com>
2020-08-05 00:02:45 -0600

    Updates

--- precache-manifest.0f340c2d8ee2e56a9fddd42fdc905afb.js
+++ precache-manifest.0f340c2d8ee2e56a9fddd42fdc905afb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "be9ec781bebc09fd21db480fc9fda177",
+    "revision": "f44aa911367bcc7bcd4d0d1e9a427ab9",
     "url": "/HSK-Learn/index.html"
   },
   {
-    "revision": "aa7677736609efd8935b",
+    "revision": "76d7bf479f8827237ec1",
     "url": "/HSK-Learn/static/css/main.e343cf0e.chunk.css"
   },
   {
-    "revision": "53029fc0f4e24aa9bc1c",
-    "url": "/HSK-Learn/static/js/2.6b9bd03c.chunk.js"
+    "revision": "2eba5c2113012d7e05e3",
+    "url": "/HSK-Learn/static/js/2.e50104d2.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/HSK-Learn/static/js/2.6b9bd03c.chunk.js.LICENSE.txt"
+    "url": "/HSK-Learn/static/js/2.e50104d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aa7677736609efd8935b",
-    "url": "/HSK-Learn/static/js/main.e701e78e.chunk.js"
+    "revision": "76d7bf479f8827237ec1",
+    "url": "/HSK-Learn/static/js/main.7456b675.chunk.js"
   },
   {
     "revision": "8ef074ca05aef3c58dee",
