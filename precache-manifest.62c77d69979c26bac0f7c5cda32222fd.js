6860883f4bfeb62f58b11d04b059780c2799f48f
davidfitz314 <davidfitz314@gmail.com>
2020-08-10 00:21:48 -0600

    Updates

--- precache-manifest.62c77d69979c26bac0f7c5cda32222fd.js
+++ precache-manifest.62c77d69979c26bac0f7c5cda32222fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2567897c9f80eaa2cbd53867e4276f0d",
+    "revision": "1163386aa8c986cecb14d90ae058560a",
     "url": "/HSK-Learn/index.html"
   },
   {
-    "revision": "69abeb0265b34747c347",
+    "revision": "81d4863a6c73f5c07a3b",
     "url": "/HSK-Learn/static/css/main.60bb5a39.chunk.css"
   },
   {
-    "revision": "826b604a402860c8688d",
-    "url": "/HSK-Learn/static/js/2.fd33be7a.chunk.js"
+    "revision": "c92f2b8df48900bd32fd",
+    "url": "/HSK-Learn/static/js/2.4378c6e7.chunk.js"
   },
   {
     "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/HSK-Learn/static/js/2.fd33be7a.chunk.js.LICENSE.txt"
+    "url": "/HSK-Learn/static/js/2.4378c6e7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "69abeb0265b34747c347",
-    "url": "/HSK-Learn/static/js/main.fcac58fd.chunk.js"
+    "revision": "81d4863a6c73f5c07a3b",
+    "url": "/HSK-Learn/static/js/main.f76afd3b.chunk.js"
   },
   {
     "revision": "8ef074ca05aef3c58dee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HSK-Learn/precache-manifest.43fbf2414789d8f118af1e4b94e272ec.js"
+  "/HSK-Learn/precache-manifest.62c77d69979c26bac0f7c5cda32222fd.js"
 );
 
 self.addEventListener('message', (event) => {
